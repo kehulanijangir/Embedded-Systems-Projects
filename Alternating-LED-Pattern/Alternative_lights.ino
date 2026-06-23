9edc6e37943ab75fa1450baa528fe2e65ade3f03
kehulanijangir <kehulanijangir84@gmail.com>
2026-06-23 19:23:17 +0530

    Add files via upload

---
+++ Alternating-LED-Pattern/Alternative_lights.ino
@@ -0,0 +1,66 @@
+void setup()
+{
+  pinMode(7,1);
+  pinMode(13,1);
+  pinMode(12,1);
+  pinMode(8,1);
+  pinMode(5,1);
+  pinMode(4,1);
+  pinMode(3,1);
+  pinMode(2,1);
+}
+
+void loop()
+{
+  digitalWrite(12,1);
+  delay(100);
+  
+  digitalWrite(8,1);
+  delay(100);
+  
+  digitalWrite(4,1);
+  delay(100);
+  
+  digitalWrite(3,1);
+  delay(100);
+  
+  digitalWrite(12,0);
+  delay(100);
+  
+  digitalWrite(8,0);
+  delay(100);
+  
+  digitalWrite(4,0);
+  delay(100);
+  
+  digitalWrite(3,0);
+  delay(100);
+  
+  digitalWrite(13,1);
+  delay(100);
+ 
+  digitalWrite(7,1);
+  delay(100);
+  
+  digitalWrite(2,1);
+  delay(100);
+  
+  digitalWrite(5,1);
+  delay(100);
+  
+   digitalWrite(13,0);
+  delay(100);
+  
+  digitalWrite(7,0);
+  delay(100);
+  
+  digitalWrite(2,0);
+  delay(100);
+  
+  digitalWrite(5,0);
+  delay(100);
+  
+  }
+  
+  
+  
